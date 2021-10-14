068efdff2be25f17fa187baf095451e85ebafec1
Zaphkiel <om4r4rm4@gmail.com>
2021-10-14 11:08:20 -0700

    Conect to mongo db atlas

--- src/index.js
+++ src/index.js
@@ -4,5 +4,3 @@ import './database';
 app.listen(app.get('PORT'), () => {
     console.log(`Server on port ${app.get('PORT')}`)
 })
-
-// cd D:\Escritorio\Auth-Basic\
--- src/utils/config.js
+++ src/utils/config.js
@@ -3,9 +3,9 @@ const gConfig = {
 		secret: process.env.JWT_SECRET || 'Zaph1312',
 	},
 	DB: {
-		URL: process.env.DB_URL || 'mongodb://Localhost/zaph_auth_api',
-		USER: process.env.DB_USER,
-		PASSWORD: process.env.DB_PASSWORD,
+		URL: process.env.DB_URI || 'mongodb://Localhost/zaph_auth_api',
+		// USER: process.env.DB_USER,
+		// PASSWORD: process.env.DB_PASSWORD,
 	},
 };
 
